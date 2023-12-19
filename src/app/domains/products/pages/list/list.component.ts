dcd6934ed7ef03b0290e5c9c102325c5bf4e2dd8
johand3v <johan.d3v@gmail.com>
2023-12-19 01:18:04 -0500

    products

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import {ProductComponent} from '../../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../../shared/components/counter/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,36 @@ import { CommonModule } from '@angular/common';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor(){
+    const initProducts:Product[]  = [
+      {
+        id:Date.now(),
+        title:'Producto 1',
+        price:100,
+        image:'https://picsum.photos/640/640?r=23',
+        creationAt:new Date().toISOString()
+      },
+      {
+        id:Date.now(),
+        title:'Producto 2',
+        price:100,
+        image:'https://picsum.photos/640/640?r=12',
+        creationAt:new Date().toISOString()
+      },
+      {
+        id:Date.now(),
+        title:'Producto 3',
+        price:100,
+        image:'https://picsum.photos/640/640?r=123',
+        creationAt:new Date().toISOString()
+      },
+    ];
+    this.products.set(initProducts);
+  }
+
 
   fromChild(event:string) {
     console.log('estamos en el padre')
--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChange } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -11,6 +11,8 @@ export class CounterComponent {
 
   @Input({required:true}) duration = 0;
   @Input({required:true}) message = '';
+  counter = signal(0);
+  counterRef:number = 0;
 
 
   constructor(){
@@ -21,11 +23,15 @@ export class CounterComponent {
     console.log('-'.repeat(10));
   }
 
-  ngOnChanges(changes : SimpleChange){
+  ngOnChanges(changes : SimpleChanges){
     //before and during render
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+    const duration = changes['duration'];
+      if(duration){
+        this.doSomething();
+      }
   }
 
   ngOnInit(): void {
@@ -37,7 +43,33 @@ export class CounterComponent {
     console.log('-'.repeat(10));
     console.log('duration =>', this.duration);
     console.log('message =>', this.message);
+    this.counterRef = window.setInterval(()=> {
+      console.log('run interval')
+      this.counter.update(statePrev => statePrev + 1 );
+    }, 1000)
   }
+
+ngAfterViewInit(): void {
+  //after render
+  //una vez
+  //async, then, subs
+  //hijos ya fueron pintados
+  console.log('ngAfterViewInit');
+  console.log('-'.repeat(10));
+}
+
+ngOnDestroy(): void {
+  //before render
+  //una vez
+  //async, then, subs
+  console.log('ngOnDestroy');
+  console.log('-'.repeat(10));
+  window.clearInterval(this.counterRef);
+}
+
+doSomething(){
+  console.log('change duration');
+}
 
 
 
